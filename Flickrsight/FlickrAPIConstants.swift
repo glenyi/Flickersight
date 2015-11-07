7a25d5c3abff45c93657c2405f0e807daaa7a454
Glen Yi <glenyi81@gmail.com>
2015-11-07 14:18:18 -0500

    - Added FlickrAPIConstants

--- Flickrsight/FlickrAPIConstants.swift
+++ Flickrsight/FlickrAPIConstants.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2015 On The Pursuit Inc. All rights reserved.
 //
 
-import Foundation
+
+// MARK: Authentication
+
+let FlickrAPIKey = "1ca511884538073ab81225b018981457"
+let FlickrAPISecret = "cfb9f3bf61bdb176"
+
+
+// MARK: Paths
+
+let FlickrAPIBaseURL = "https://api.flickr.com/services/rest"
+
+
+// MARK: Methods
+
+let FlickrAPIMethodSearch = "flickr.photos.search"
+
+
+// MARK: Params
+
+let FlickrAPIParamDefaults = "page=1&format=json&nojsoncallback=1"
+let FlickrAPIParamApiKey = "api_key"
+let FlickrAPIParamText = "text"
+let FlickrAPIParamTag = "tag"
+let FlickrAPIPerPage = "per_page"
+
